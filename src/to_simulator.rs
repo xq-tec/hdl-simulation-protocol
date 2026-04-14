68da3c777c40b3b532471468d81a07c20c44bd7b
Kai Giebeler <giebeler@xq-tec.com>
2026-04-15 00:25:09 +0200

    fix command serialization

--- src/to_simulator.rs
+++ src/to_simulator.rs
@@ -4,12 +4,18 @@ use serde::Serialize;
 use crate::design_hierarchy::SignalElementId;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub enum Command {
+    #[serde(rename_all = "camelCase")]
     StartSimulation,
+    #[serde(rename_all = "camelCase")]
     PauseSimulation,
+    #[serde(rename_all = "camelCase")]
     ResumeSimulation,
+    #[serde(rename_all = "camelCase")]
     StopSimulation,
 
+    #[serde(rename_all = "camelCase")]
     TrackSignals(SignalTrackingRequest),
 }
 
